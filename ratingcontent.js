c6b249199a68c5303becf557457c7bcbaeb38b17
mstein166 <53192833+mstein166@users.noreply.github.com>
2024-03-31 21:37:56 -0700

    Update ratingcontent.js

--- ratingcontent.js
+++ ratingcontent.js
@@ -11,22 +11,30 @@ const title = document.querySelector('h1.headline-1');
 
 
 //pass letterboxd movie title to TMDB API to retrieve user score
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YmEzZmJjZjA5NzMxNzZkYTYxM2U1YTNjNjdlY2RjMiIsInN1YiI6IjYyZWM5NTVmZWE4NGM3MDA1ZWNmYjhlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jIRVqNSgLZsrV9torSrJ9gCSWoaPBYipdqPg87V7JLk'
+const apiKey = '4ba3fbcf0973176da613e5a3c67ecdc2'
+
+async function fetchMovieIdByTitle(movieTitle) {
+  try {
+    const encodedTitle = encodeURIComponent(movieTitle);
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodedTitle}`);
+
+    if (!response.ok) {
+      throw new Error('API request failed');
+    }
+
+    // Parse the JSON response
+    const data = await response.json();
+
+    if (data.results.length > 0) {
+      // Assuming the first result is the desired movie
+      const movieId = data.results[0].id;
+      return movieId;
+      console.log(movieId);
+    } else {
+      throw new Error('No results found');
+    }
+  } catch (error) {
+    console.error('Failed to fetch movie ID:', error.message);
+    return null;
   }
-};
-
-fetch('https://api.themoviedb.org/3/search/movie?query={#movieTitle)&include_adult=false&language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-//trying to set integer to ID from query
-  int movieID = id;
-
-fetch('https://api.themoviedb.org/3/movie/{#movieID}?language=en-US', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+}
