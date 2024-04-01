4c7e35e8ad71b5d3588564f81bd6c9b4be19bfb2
mstein166 <53192833+mstein166@users.noreply.github.com>
2024-03-31 21:21:46 -0700

    Update ratingcontent.js

--- ratingcontent.js
+++ ratingcontent.js
@@ -11,3 +11,22 @@ const title = document.querySelector('h1.headline-1');
 
 
 //pass letterboxd movie title to TMDB API to retrieve user score
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YmEzZmJjZjA5NzMxNzZkYTYxM2U1YTNjNjdlY2RjMiIsInN1YiI6IjYyZWM5NTVmZWE4NGM3MDA1ZWNmYjhlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jIRVqNSgLZsrV9torSrJ9gCSWoaPBYipdqPg87V7JLk'
+  }
+};
+
+fetch('https://api.themoviedb.org/3/search/movie?query={#movieTitle)&include_adult=false&language=en-US&page=1', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+//trying to set integer to ID from query
+  int movieID = id;
+
+fetch('https://api.themoviedb.org/3/movie/{#movieID}?language=en-US', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
